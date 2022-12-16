00048b377c0d23403b472bf8d819965c60e140a3
Attila Starkenius <symphtom@hotmail.com>
2022-12-16 09:50:54 +0100

    Add project files.

---
+++ MVCIntro/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCIntro.Models;
+using System.Diagnostics;
+
+namespace MVCIntro.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public HomeController()
+        {
+
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            //Console.WriteLine("Någon klickade på Privacy-länken!");
+            //return View(Index);
+            return View(nameof(Index));
+        }
+
+        public void
+    }
+}
